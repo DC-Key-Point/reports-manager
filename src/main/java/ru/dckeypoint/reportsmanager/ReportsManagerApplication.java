93a2415684b7dfaeb53f851bf2678ffb228e0982
Poke0647 <82381868+Poke0647@users.noreply.github.com>
2025-05-22 20:31:51 +1000

    create

---
+++ src/main/java/ru/dckeypoint/reportsmanager/ReportsManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.dckeypoint.reportsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportsManagerApplication.class, args);
+    }
+
+}
